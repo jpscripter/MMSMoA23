4e387f0c6ca022ea0def9f4529828b2493796cdc
JPScripter <JPScripter@gmail.com>
2023-04-30 13:16:24 -0500

    remoting

--- PowerShell, PowerFull/06 - PSRemoting.ps1
+++ PowerShell, PowerFull/06 - PSRemoting.ps1
@@ -1,3 +1,13 @@
 # As Admin
 $Cred = Get-Credential -UserName Corp\LabAdmin
 $PSSession = New-PSSession -VMName HYD-CM1 -Credential $Cred
+$PSSession.Runspace
+import-module C:\Users\jpscr\Repos\WindowManagement\WindowManagement.psd1
+$powershell = [Powershell]::Create()
+$Powershell.Runspace = $PSSession.Runspace
+$Powershell.Commands.AddScript('Whoami; $Env:computername',$True)
+$powershell.Invoke()
+
+invoke-command -Session $PSSession -ScriptBlock {$Using:cred|gm}
+
+Enter-PSSession -Session $PSSession
