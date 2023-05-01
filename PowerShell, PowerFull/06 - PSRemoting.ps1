aea269c5925d39c1d833e0235665f5f1164752e0
JPScripter <JPScripter@gmail.com>
2023-05-01 18:36:12 -0500

    remoting

--- PowerShell, PowerFull/06 - PSRemoting.ps1
+++ PowerShell, PowerFull/06 - PSRemoting.ps1
@@ -18,3 +18,13 @@ Enter-PSSession -Session $PSSession
 
 Copy-Item -Path C:\Users\jpscr\Repos\WindowManagement\ -Recurse -Destination c:\repos\ -ToSession $PSSession
 
+
+$PSSessionGW = New-PSSession -VMName HYD-GW1 -Credential $Cred
+invoke-command -Session $PSSessionGW -ScriptBlock {
+    $json = get-content -path C:\Windows\Temp\CMScriptConnection.txt -Raw
+    $connectionDetails = convertfrom-json $json
+    Enter-PSHostProcess -Id $connectionDetails.pid
+    #Get-Runspace
+    #Debug-Runspace -Id $connectionDetails.runspace
+}
+Enter-PSSession -Session $PSSessionGW
